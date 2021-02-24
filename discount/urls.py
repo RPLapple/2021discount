a2651a8a179100dc0107070a54a91cd371cc53fa
RPL <rpl@RPLde-Air.fritz.box>
2021-02-24 10:24:48 +0100

    update models.py, urls.py

---
+++ discount/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as view_auth
+from discount.views import register, index
+
+# 連接網址
+urlpatterns = [
+    path('', index),
+    path('works', views.works, name='works'),
+    path('work_discount', views.work_discount, name='work_discount'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('myaccount', views.myaccount, name='myaccount'),
+    # 這啥= =?
+    path('table', views.table, name='table'),
+    path('accounts/register/', register),
+    path('accounts/login/', view_auth.LoginView.as_view(), name='login'),
+    path('accounts/logout', view_auth.LogoutView.as_view(template_name='index.html'), name='logout'),
+    path('accounts/profile/', index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- discount/views.py
+++ discount/views.py
@@ -1,4 +1,87 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http.response import JsonResponse
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
+from datetime import datetime
+from .models import Product, Card
 
 
 # Create your views here.
+# urls.py那邊的 等等要刪掉
+# from discount.views import register, index
+# homepage/indexpage
+def index(request):
+    return render(request, 'index.html', {
+        'current_time': str(datetime.now()),
+    })
+
+
+# all projects
+def works(request):
+    return render(request, 'works.html')
+
+
+# discount page
+def work_discount(request):
+    return render(request, 'work_discount.html')
+
+
+# 這邊要再看一次
+# table是用來幹嘛的＝＝？
+# show 資料在額外頁面
+def table(request):
+    data = Product.objects.values(
+        'supermarket__name',
+        'name',
+        'discount',
+        'new_price',
+        'old_price',
+        'discount__card__name',
+        'discount__extradiscount__name',
+    )
+    return JsonResponse(list(data), safe=False)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def myaccount(request):
+    return render(request, 'myaccount.html')
+
+
+# 這邊要再看一次, 還有render的最後一個值是什麼？
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return HttpResponseRedirect('/account/login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', locals())
+
+
+# 這邊要再看一次, 還有render的最後一個值是什麼？
+def login(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.ise_active:
+        auth.login(request, user)
+        return render(request, 'index.html')
+    else:
+        return render(request, 'registration/login.html', locals())
+
+
+# 這邊要再看一次
+def logout(request):
+    auth.logout(request)
+    return render(request, 'index.html')
